fc60510f65f70071e7a3834ff6b61a4d678e7490
Roman Kopylov <romankopilov1712@gmail.com>
2023-06-11 17:12:02 +0300

    strings

--- variables.py
+++ variables.py
@@ -5,6 +5,17 @@ print(id(a))  # 2780091929392 - создается новый объект
 a = a.upper()
 print(id(a))  # 1395174756912 - новый, переменная выступает как ярлык, который ссылается на объект
 
-result = 'hello {}'
+result = 'hello {}'  # можно через {}
 s = result.format(a)
 print(s)
+
+# another way
+name = 'Perception'
+views = 100
+onWebsite = '{filmName} + {viewsNumber}'
+result = onWebsite.format(filmName = name,viewsNumber = views)
+print(result)
+
+# another way
+result2 = f'{name} + {views}'
+print(result2)
